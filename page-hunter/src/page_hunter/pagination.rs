2cabeca3c7916d4e51a55f1ce9dbb54ff8b1d016
JMTamayo <jmtamayog23@gmail.com>
2024-05-19 22:53:48 -0500

    Implementation of the **PgSqlxPagination** for . Removing Debug implementation from bind_records and paginate_records

--- page-hunter/src/page_hunter/pagination.rs
+++ page-hunter/src/page_hunter/pagination.rs
@@ -1,6 +1,12 @@
-use std::fmt::Debug;
+use std::future::Future;
 
 use super::models::*;
+
+#[cfg(any(feature = "pg-sqlx"))]
+use sqlx::{query, query_builder::QueryBuilder, query_scalar, FromRow};
+
+#[cfg(feature = "pg-sqlx")]
+use sqlx::postgres::{PgPool, PgRow, Postgres};
 
 /// Paginate records into a [`Page`] model.
 /// #### Arguments:
@@ -25,7 +31,7 @@ use super::models::*;
 pub fn paginate_records<R>(records: &R, page: usize, size: usize) -> PaginationResult<Page<R::Item>>
 where
     R: IntoIterator + Clone,
-    R::Item: Clone + Debug,
+    R::Item: Clone,
 {
     Ok(Page::new(
         &records
@@ -60,7 +66,7 @@ where
 pub fn bind_records<R>(records: &R, size: usize) -> PaginationResult<Book<R::Item>>
 where
     R: IntoIterator + Clone,
-    R::Item: Clone + Debug,
+    R::Item: Clone,
 {
     let total: usize = records.clone().into_iter().count();
 
@@ -86,4 +92,62 @@ where
             })
             .collect::<PaginationResult<Vec<Page<R::Item>>>>()?,
     ))
+}
+
+/// Paginate results from a SQL query into a [`Page`] model from a PostgreSQL database using [`sqlx`].
+/// Only available when the `pg-sqlx` feature is enabled.
+#[cfg(feature = "pg-sqlx")]
+pub trait PgSqlxPagination {
+    fn paginate<'p, S>(
+        &self,
+        pool: &'p PgPool,
+        page: usize,
+        size: usize,
+    ) -> impl Future<Output = PaginationResult<Page<S>>>
+    where
+        S: for<'r> FromRow<'r, PgRow> + Clone;
+}
+
+/// Implementation of the [`PgSqlxPagination`] trait for the [`QueryBuilder`] struct from [`sqlx`].
+/// Only available when the `pg-sqlx` feature is enabled.
+#[cfg(feature = "pg-sqlx")]
+impl<'q> PgSqlxPagination for QueryBuilder<'q, Postgres> {
+    async fn paginate<'p, S>(
+        &self,
+        pool: &'p PgPool,
+        page: usize,
+        size: usize,
+    ) -> PaginationResult<Page<S>>
+    where
+        S: for<'r> FromRow<'r, PgRow> + Clone,
+    {
+        let total: i64 = query_scalar(
+            QueryBuilder::<Postgres>::new(format!(
+                "WITH temp_table AS ({}) SELECT count(*) from temp_table;",
+                self.sql()
+            ))
+            .sql(),
+        )
+        .fetch_one(pool)
+        .await?;
+
+        let rows: Vec<PgRow> = query(
+            QueryBuilder::<Postgres>::new(format!(
+                "WITH temp_table AS ({}) SELECT * from temp_table LIMIT {} OFFSET {};",
+                self.sql(),
+                size,
+                size * page,
+            ))
+            .sql(),
+        )
+        .fetch_all(pool)
+        .await?;
+
+        let items: Vec<S> = rows
+            .into_iter()
+            .map(|row| S::from_row(&row))
+            .collect::<Result<Vec<S>, _>>()?;
+
+        Ok(Page::new(&items, page, size, total as usize)?)
+    }
 }
