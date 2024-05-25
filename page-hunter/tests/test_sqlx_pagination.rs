08b1df1f20a6bac9f2bf90440a7c9f95d5bbe02b
JMTamayo <jmtamayog23@gmail.com>
2024-05-24 21:46:52 -0500

    Including tests to verify FromRow error in pagination

--- page-hunter/tests/test_sqlx_pagination.rs
+++ page-hunter/tests/test_sqlx_pagination.rs
@@ -130,6 +130,59 @@ pub mod test_postgres_pagination {
         )
     }
 
+    /// Test from row error when is not possible to parse the result
+    #[tokio::test]
+    async fn test_from_row_error() {
+        use sqlx::postgres::PgPoolOptions;
+        use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
+        use time::OffsetDateTime;
+        use uuid::Uuid;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("PG_DB_PORT").expect("PG_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct User {
+            id: Uuid,
+            username: String,
+            hashed_password: String,
+            age: i32,
+            is_active: bool,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: PgPool = match PgPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "postgres://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to Postgres: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<Postgres> =
+            QueryBuilder::<Postgres>::new("SELECT * FROM test_page_hunter.users");
+
+        let users_pagination: PaginationResult<Page<User>> = query.paginate(&pool, 2, 3).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "FROM ROW ERROR- no column found for name: age".to_string(),
+        )
+    }
+
     /// Test pagination with invalid page
     #[tokio::test]
     async fn test_pagination_invalid_page() {
@@ -305,6 +358,56 @@ pub mod test_mysql_pagination {
             error,
             "DATABASE ERROR- error returned from database: 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ';) as temp_table' at line 1"
                 .to_string(),
+        )
+    }
+
+    /// Test from row error when is not possible to parse the result
+    #[tokio::test]
+    async fn test_from_row_error() {
+        use sqlx::mysql::MySqlPoolOptions;
+        use sqlx::{FromRow, MySql, MySqlPool, QueryBuilder};
+        use time::OffsetDateTime;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("MYSQL_DB_PORT").expect("MYSQL_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct States {
+            id: i64,
+            country_name: String,
+            name: String,
+            extension: i32,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: MySqlPool = match MySqlPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "mysql://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to MySQL: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<MySql> = QueryBuilder::<MySql>::new("SELECT * FROM states");
+
+        let users_pagination: PaginationResult<Page<States>> = query.paginate(&pool, 4, 7).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "FROM ROW ERROR- no column found for name: extension".to_string(),
         )
     }
 
