e0d49755798658c86ba7404ace3e0ee410ac8f7b
JMTamayo <jmtamayog23@gmail.com>
2024-05-24 20:55:11 -0500

    Including tests to verify error fetching total records. Including tests to verify field value error by invalid page index

--- page-hunter/tests/test_sqlx_pagination.rs
+++ page-hunter/tests/test_sqlx_pagination.rs
@@ -5,8 +5,9 @@ pub mod test_postgres_pagination {
     use page_hunter::*;
     use std::env;
 
+    /// Test successful pagination
     #[tokio::test]
-    async fn test_bind_records_from_postgres_db_with_sqlx() {
+    async fn test_pagination_success() {
         use sqlx::postgres::PgPoolOptions;
         use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
         use time::OffsetDateTime;
@@ -75,6 +76,111 @@ pub mod test_postgres_pagination {
         assert!(users.get_items()[1].updated_at.is_none());
         assert!(users.get_items()[2].updated_at.is_none());
     }
+
+    /// Test database error when is not possible to get total by invalid query
+    #[tokio::test]
+    async fn test_error_fetching_total_records() {
+        use sqlx::postgres::PgPoolOptions;
+        use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
+        use time::OffsetDateTime;
+        use uuid::Uuid;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("PG_DB_PORT").expect("PG_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct User {
+            id: Uuid,
+            username: String,
+            hashed_password: String,
+            is_active: bool,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: PgPool = match PgPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "postgres://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to Postgres: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<Postgres> =
+            QueryBuilder::<Postgres>::new("SELECT * FROM test_page_hunter.users;");
+
+        let users_pagination: PaginationResult<Page<User>> = query.paginate(&pool, 2, 3).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "DATABASE ERROR- error returned from database: syntax error at or near \";\""
+                .to_string(),
+        )
+    }
+
+    /// Test pagination with invalid page
+    #[tokio::test]
+    async fn test_pagination_invalid_page() {
+        use sqlx::postgres::PgPoolOptions;
+        use sqlx::{FromRow, PgPool, Postgres, QueryBuilder};
+        use time::OffsetDateTime;
+        use uuid::Uuid;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("PG_DB_PORT").expect("PG_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct User {
+            id: Uuid,
+            username: String,
+            hashed_password: String,
+            is_active: bool,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: PgPool = match PgPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "postgres://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to Postgres: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<Postgres> =
+            QueryBuilder::<Postgres>::new("SELECT * FROM test_page_hunter.users");
+
+        let users_pagination: PaginationResult<Page<User>> = query.paginate(&pool, 5, 30).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "FIELD VALUE ERROR- Page index '5' exceeds total pages '4'".to_string(),
+        )
+    }
 }
 
 #[cfg(feature = "mysql-sqlx")]
@@ -83,8 +189,9 @@ pub mod test_mysql_pagination {
     use page_hunter::*;
     use std::env;
 
+    /// Test successful pagination
     #[tokio::test]
-    async fn test_bind_records_from_mysql_db_with_sqlx() {
+    async fn test_pagination_success() {
         use sqlx::mysql::MySqlPoolOptions;
         use sqlx::{FromRow, MySql, MySqlPool, QueryBuilder};
         use time::OffsetDateTime;
@@ -149,5 +256,104 @@ pub mod test_mysql_pagination {
         assert_eq!(users.get_items()[4].name, "State 33");
         assert_eq!(users.get_items()[5].name, "State 34");
         assert_eq!(users.get_items()[6].name, "State 35");
+    }
+
+    /// Test database error when is not possible to get total by invalid query
+    #[tokio::test]
+    async fn test_error_fetching_total_records() {
+        use sqlx::mysql::MySqlPoolOptions;
+        use sqlx::{FromRow, MySql, MySqlPool, QueryBuilder};
+        use time::OffsetDateTime;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("MYSQL_DB_PORT").expect("MYSQL_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct States {
+            id: i64,
+            country_name: String,
+            name: String,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: MySqlPool = match MySqlPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "mysql://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to MySQL: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<MySql> = QueryBuilder::<MySql>::new("SELECT * FROM states;");
+
+        let users_pagination: PaginationResult<Page<States>> = query.paginate(&pool, 4, 7).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "DATABASE ERROR- error returned from database: 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ';) as temp_table' at line 1"
+                .to_string(),
+        )
+    }
+
+    /// Test pagination with invalid page
+    #[tokio::test]
+    async fn test_pagination_invalid_page() {
+        use sqlx::mysql::MySqlPoolOptions;
+        use sqlx::{FromRow, MySql, MySqlPool, QueryBuilder};
+        use time::OffsetDateTime;
+
+        let db_host: String = env::var("DB_HOST").expect("DB_HOST var not found");
+        let db_port: String = env::var("MYSQL_DB_PORT").expect("MYSQL_DB_PORT var not found");
+        let db_user: String = env::var("DB_USER").expect("DB_USER var not found");
+        let db_password: String = env::var("DB_PASSWORD").expect("DB_PASSWORD var not found");
+        let db_name: String = env::var("DB_NAME").expect("DB_NAME var not found");
+
+        #[derive(Clone, Debug, FromRow)]
+        #[allow(dead_code)]
+        pub struct States {
+            id: i64,
+            country_name: String,
+            name: String,
+            created_at: OffsetDateTime,
+            updated_at: Option<OffsetDateTime>,
+        }
+
+        let pool: MySqlPool = match MySqlPoolOptions::new()
+            .max_connections(1)
+            .connect(&format!(
+                "mysql://{}:{}@{}:{}/{}",
+                db_user, db_password, db_host, db_port, db_name
+            ))
+            .await
+        {
+            Ok(pool) => pool,
+            Err(e) => {
+                panic!("Failed to connect to MySQL: {:?}", e);
+            }
+        };
+
+        let query: QueryBuilder<MySql> = QueryBuilder::<MySql>::new("SELECT * FROM states");
+
+        let users_pagination: PaginationResult<Page<States>> = query.paginate(&pool, 5, 30).await;
+        assert!(users_pagination.is_err());
+
+        let error: String = users_pagination.unwrap_err().to_string();
+        assert_eq!(
+            error,
+            "FIELD VALUE ERROR- Page index '5' exceeds total pages '4'".to_string(),
+        )
     }
 }
