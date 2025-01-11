dbdf3b0acc48dbc63c25ffd2c70022075d3781dd
JMTamayo <jmtamayog23@gmail.com>
2025-01-10 19:53:20 -0500

    Update Axum  example according to the latest changes from page-hunter

--- examples/axum/src/api/routes/docs.rs
+++ examples/axum/src/api/routes/docs.rs
@@ -5,27 +5,16 @@ use utoipa_swagger_ui::SwaggerUi;
 use crate::api::router::ServiceRouter;
 use crate::docs::open_api::ApiDoc;
 
-pub struct DocsRouter {
-    path_base: String,
-}
+pub struct DocsRouter;
 
 impl ServiceRouter for DocsRouter {
     fn new() -> Self {
-        Self {
-            path_base: String::from(""),
-        }
-    }
-
-    fn get_path_base(&self) -> &str {
-        &self.path_base
+        Self
     }
 
     fn get_router(&self) -> Router {
-        Router::new().nest(
-            self.get_path_base(),
-            Router::new().merge(
-                SwaggerUi::new("/swagger-ui").url("/docs/api-docs/openapi.json", ApiDoc::openapi()),
-            ),
-        )
+        Router::new().merge(Router::new().merge(
+            SwaggerUi::new("/swagger-ui").url("/docs/api-docs/openapi.json", ApiDoc::openapi()),
+        ))
     }
 }
